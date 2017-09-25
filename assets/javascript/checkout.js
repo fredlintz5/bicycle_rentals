6e0595d1257c7439a93fefd5bc5936fc6e499552
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-09-25 08:31:51 -0600

    added paypal checkout button

---
+++ assets/javascript/checkout.js
@@ -0,0 +1,46 @@
+console.log('checkout loaded');
+
+paypal.Button.render({
+
+    env: 'sandbox', // Or 'production'
+
+    client: {
+        sandbox:    'AS_aVQen4-2M2lWhNQaKxSUrZ2GAJMy74mcWxFT7rw_ypPDtrAREwFRZZvZBQ94zZC9y-zoAcWqw3mCl',
+        production: 'AS_aVQen4-2M2lWhNQaKxSUrZ2GAJMy74mcWxFT7rw_ypPDtrAREwFRZZvZBQ94zZC9y-zoAcWqw3mCl'
+    },
+
+    style: {
+        label: 'checkout',
+        size: 'medium',
+        color: 'blue'
+    },
+
+    commit: true, // Show a 'Pay Now' button
+
+    payment: function(data, actions) {
+        return actions.payment.create({
+            payment: {
+                transactions: [
+                    {
+                        amount: { total: '1.00', currency: 'USD' }
+                    }
+                ]
+            }
+        });
+    },
+
+    onAuthorize: function(data, actions) {
+        return actions.payment.execute().then(function(payment) {
+
+            // The payment is complete!
+            // You can now show a confirmation message to the customer
+        });
+    }
+
+}, '#paypal-button');
+
+
+
+
+
+
